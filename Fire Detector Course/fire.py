f8338a420197c9791c4896fc790e2f0add08f4f9
Nanor Felix <109562442+Tech-Watt@users.noreply.github.com>
2023-03-01 04:12:11 +0000

    Add files via upload

---
+++ Fire Detector Course/fire.py
@@ -0,0 +1,40 @@
+from ultralytics import YOLO
+import cvzone
+import cv2
+import math
+
+
+
+
+# Running real time from webcam
+cap = cv2.VideoCapture('fire2.mp4')
+model = YOLO('fire.pt')
+
+
+# Reading the classes
+classnames = ['fire']
+
+while True:
+    ret,frame = cap.read()
+    frame = cv2.resize(frame,(640,480))
+    result = model(frame,stream=True)
+
+    # Getting bbox,confidence and class names informations to work with
+    for info in result:
+        boxes = info.boxes
+        for box in boxes:
+            confidence = box.conf[0]
+            confidence = math.ceil(confidence * 100)
+            Class = int(box.cls[0])
+            if confidence > 50:
+                x1,y1,x2,y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),5)
+                cvzone.putTextRect(frame, f'{classnames[Class]} {confidence}%', [x1 + 8, y1 + 100],
+                                   scale=1.5,thickness=2)
+
+
+
+
+    cv2.imshow('frame',frame)
+    cv2.waitKey(1)
